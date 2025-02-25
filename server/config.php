77c54cd172df7643c6683b47aa9079be165cf9ca
simongc04 <simonescuela9@gmail.com>
2025-02-25 13:08:19 +0000

    token agregado

--- server/config.php
+++ server/config.php
@@ -2,6 +2,6 @@
 // Configuración de conexión y globales
 define('WSDL_URL', '../wsdl/centro.wsdl');
 define('LOG_FILE', '../logs/soap.log');
-define('ADMIN_TOKEN', '');
+define('ADMIN_TOKEN', '017e8197922ee2bc88fac806cd03da0c');
 define('','');
 ?>

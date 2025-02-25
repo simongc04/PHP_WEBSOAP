3ef7a4ed14a96d1c6afafc9a1e8dc11f2db819a6
Airam De León <airam.deleon@akacenter.com>
2025-02-25 13:27:05 +0000

    MoodleUser Created | still need crud

--- server/config.php
+++ server/config.php
@@ -4,4 +4,8 @@ define('WSDL_URL', '../wsdl/centro.wsdl');
 define('LOG_FILE', '../logs/soap.log');
 define('ADMIN_TOKEN', '017e8197922ee2bc88fac806cd03da0c');
 define('','');
+
+define('MOODLE_HOST', 'https://teleformacion.akacenter.es');
+define('MOODLE_API_REST', '/webservice/rest/server.php');
+define('MOODLE_TOKEN', '017e8197922ee2bc88fac806cd03da0c');
 ?>
---
+++ server/moodleUser.php
@@ -0,0 +1,50 @@
+<?php
+require_once 'config.php';
+
+class MoodleClient {
+    private $host;
+    private $apiRest;
+    private $token;
+
+    public function __construct() {
+        $this->host = MOODLE_HOST;
+        $this->apiRest = MOODLE_API_REST;
+        $this->token = MOODLE_TOKEN;
+    }
+
+    /**
+     * Método genérico para hacer solicitudes a la API REST de Moodle
+     */
+    private function callMoodleAPI($function, $params) {
+        $params['wstoken'] = $this->token;
+        $params['wsfunction'] = $function;
+        $params['moodlewsrestformat'] = 'json';
+
+        $url = $this->host . $this->apiRest . '?' . http_build_query($params);
+
+        $response = file_get_contents($url);
+
+        if ($response === false) {
+            return ['codigo' => -2, 'mensaje' => 'WS no disponible.'];
+        }
+
+        $data = json_decode($response, true);
+
+        if (isset($data['exception'])) {
+            return ['codigo' => -1, 'mensaje' => 'Error inesperado: ' . $data['message']];
+        }
+
+        return $data;
+    }
+
+    
+    public function obtenerUsuarioPorId($userId) {
+        $params = [
+            'field' => 'id',
+            'values[0]' => $userId
+        ];
+
+        return $this->callMoodleAPI('core_user_get_users_by_field', $params);
+    }
+}
+?>

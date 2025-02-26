34bbd57048b457cbd225a1da0aedc15797a46e34
simongc04 <simonescuela9@gmail.com>
2025-02-26 08:10:05 +0000

    actualizado

--- server/config.php
+++ server/config.php
@@ -7,5 +7,5 @@ define('','');
 
 define('MOODLE_HOST', 'https://teleformacion.akacenter.es');
 define('MOODLE_API_REST', '/webservice/rest/server.php');
-define('MOODLE_TOKEN', '017e8197922ee2bc88fac806cd03da0c');
+define('MOODLE_TOKEN', '797ddf121fdbc5bf8f06f2668c24dbea');
 ?>

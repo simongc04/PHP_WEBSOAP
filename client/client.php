44cf7586dc5f28bddd68590b836b8d030cf655bd
simongc04 <simonescuela9@gmail.com>
2025-02-26 08:09:21 +0000

    actualizado

--- client/client.php
+++ client/client.php
@@ -8,7 +8,7 @@ $options = [
 $client = new SoapClient('../wsdl/centro.wsdl', $options);
 
 $request = [
-    'TOKEN' => '017e8197922ee2bc88fac806cd03da0c', // Reemplaza con tu token real
+    'TOKEN' => '797ddf121fdbc5bf8f06f2668c24dbea', // Reemplaza con tu token real
     'ID_CENTRO' => '12345',
     'NOMBRE_CENTRO' => 'Centro Ejemplo',
     'URL_PLATAFORMA' => 'https://moodle.ejemplo.com',
--- server/config.php
+++ server/config.php
@@ -2,6 +2,6 @@
 // Configuración de conexión y globales
 define('WSDL_URL', '../wsdl/centro.wsdl');
 define('LOG_FILE', '../logs/soap.log');
-define('ADMIN_TOKEN', '017e8197922ee2bc88fac806cd03da0c');
+define('ADMIN_TOKEN', '797ddf121fdbc5bf8f06f2668c24dbea');
 define('','');
 ?>
